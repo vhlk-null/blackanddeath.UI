10a51afa8e8889207e3428ff50b14299a8966b99
nullhandlex <216790482+nullhandlex@users.noreply.github.com>
2026-03-16 01:28:05 +0200

    Adding routes on header

--- blackanddeath.UI/src/app/core/components/header/header.ts
+++ blackanddeath.UI/src/app/core/components/header/header.ts
@@ -1,12 +1,14 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ThemeService } from '../../../core/services/theme.service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
   readonly theme = inject(ThemeService);
+  readonly menuOpen = signal(false);
 }
