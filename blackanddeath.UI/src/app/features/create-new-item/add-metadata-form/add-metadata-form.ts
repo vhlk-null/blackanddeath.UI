2388d13b9259843616b51f57349671a83e721888
nullhandlex <216790482+nullhandlex@users.noreply.github.com>
2026-03-17 00:19:17 +0200

    Adding new page - add metadata

---
+++ blackanddeath.UI/src/app/features/create-new-item/add-metadata-form/add-metadata-form.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-metadata-form',
+  templateUrl: './add-metadata-form.html',
+  styleUrl: './add-metadata-form.scss',
+})
+export class AddMetadataForm {}
--- blackanddeath.UI/src/app/features/create-new-item/create-new-item.ts
+++ blackanddeath.UI/src/app/features/create-new-item/create-new-item.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Section } from '../../shared/components/section/section';
 import { AddBandForm } from './add-band-form/add-band-form';
 import { AddAlbumForm } from './add-album-form/add-album-form';
+import { AddMetadataForm } from './add-metadata-form/add-metadata-form';
 
 @Component({
   selector: 'app-create-new-item',
-  imports: [Section, AddBandForm, AddAlbumForm],
+  imports: [Section, AddBandForm, AddAlbumForm, AddMetadataForm],
   templateUrl: './create-new-item.html',
   styleUrl: './create-new-item.scss',
 })
