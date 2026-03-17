7de7cc59a5c6490f193aa7f95bdb151ff212b8a8
nullhandlex <216790482+nullhandlex@users.noreply.github.com>
2026-03-17 22:00:51 +0200

    added footter

--- blackanddeath.UI/src/app/app.ts
+++ blackanddeath.UI/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Header } from "./core/components/header/header";
+import { Footer } from "./core/components/footer/footer";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Header,  RouterOutlet],
+  imports: [Header, Footer, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
