ec6fff86be0fd3b20de5e98e5527b75b7103df86
nullhandlex <216790482+nullhandlex@users.noreply.github.com>
2026-04-03 23:57:14 +0300

    changing the style of loader spinner

--- blackanddeath.UI/src/app/core/interceptors/loader.interceptor.ts
+++ blackanddeath.UI/src/app/core/interceptors/loader.interceptor.ts
@@ -1,9 +1,15 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpContextToken, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { finalize } from 'rxjs';
 import { LoaderService } from '../../shared/services/loader.service';
 
+export const SKIP_LOADER = new HttpContextToken<boolean>(() => false);
+
 export const loaderInterceptor: HttpInterceptorFn = (req, next) => {
+  if (req.context.get(SKIP_LOADER)) {
+    return next(req);
+  }
+
   const loader = inject(LoaderService);
   loader.show();
   return next(req).pipe(finalize(() => loader.hide()));
