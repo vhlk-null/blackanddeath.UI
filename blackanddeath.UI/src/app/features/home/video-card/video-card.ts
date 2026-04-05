6923a6ad8b964345791f37c0319b4eb600be3b39
nullhandlex <216790482+nullhandlex@users.noreply.github.com>
2026-04-05 18:22:49 +0300

    fixing video cards

--- blackanddeath.UI/src/app/features/home/video-card/video-card.ts
+++ blackanddeath.UI/src/app/features/home/video-card/video-card.ts
@@ -1,17 +1,28 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { VideoBand } from '../../../shared/models/video-band';
+import { SafeUrlPipe } from '../../../shared/pipes/safe-url.pipe';
 
 @Component({
   selector: 'app-video-card',
+  imports: [SafeUrlPipe],
   templateUrl: './video-card.html',
   styleUrl: './video-card.scss',
 })
 export class VideoCard {
   videoCard = input.required<VideoBand>();
+  playing = signal(false);
+
+  get youtubeId(): string | null {
+    return this.extractYoutubeId(this.videoCard().youtubeLink);
+  }
 
   get thumbnailUrl(): string {
-    const id = this.extractYoutubeId(this.videoCard().youtubeLink);
+    const id = this.youtubeId;
     return id ? `https://img.youtube.com/vi/${id}/mqdefault.jpg` : '';
+  }
+
+  get embedUrl(): string {
+    return `https://www.youtube.com/embed/${this.youtubeId}?autoplay=1`;
   }
 
   private extractYoutubeId(url: string): string | null {
