3405b7204fd8305fbccc765808b62e250e6771b1
aleksandr.medved <aleksandr.medved@nixs.com>
2026-05-05 15:59:31 +0300

    commetning the calls for notification services for now

--- blackanddeath.UI/src/app/app.config.ts
+++ blackanddeath.UI/src/app/app.config.ts
@@ -9,7 +9,7 @@ import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { AuthService } from './core/auth/auth.service';
 import { AppConfigService } from './core/services/app-config.service';
 import { initEndpoints } from './shared/constants/endpoints';
-import { NotificationService } from './features/services/notification.service';
+// import { NotificationService } from './features/services/notification.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,11 +20,11 @@ export const appConfig: ApplicationConfig = {
     provideAppInitializer(async () => {
       const config = inject(AppConfigService);
       const auth = inject(AuthService);
-      const notifications = inject(NotificationService);
+      // const notifications = inject(NotificationService);
       await config.load();
       initEndpoints(config);
       await auth.init();
-      if (auth.isAuthenticated()) await notifications.init();
+      // if (auth.isAuthenticated()) await notifications.init();
     }),
   ]
 };
